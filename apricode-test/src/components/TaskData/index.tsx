fc4789c630779bfd5bdf1a5024823f9b0ed9045e
Илья Заостровский <izaostrovskiy@mail.ru>
2024-08-06 19:46:37 +0500

    Create project

---
+++ apricode-test/src/components/TaskData/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './TaskData.module.scss';
+const TaskData = () => {
+  return (
+    <div className={styles.taskInfo}>
+      <div className={styles.title}>Зaдача 1.1</div>
+      <div className={styles.text}>
+        Ntrcntrcntrcntrcntrcntrcntrcntrcntrcntrcntrcntrcntrcntrcntrcn
+      </div>
+    </div>
+  );
+};
+
+export default TaskData;
---
+++ apricode-test/src/components/TaskTree/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import TaskTreeItem from '../';
+import styles from './TaskTree.module.scss';
+const TaskTree = () => {
+  return <div className={styles.TaskTree}>{/* <TaskTreeItem /> */}</div>;
+};
+
+export default TaskTree;
