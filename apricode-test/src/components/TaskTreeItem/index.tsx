de91af54c6deba50e5ed49f6dca1da4e1f2da99a
Илья Заостровский <izaostrovskiy@mail.ru>
2024-08-08 13:08:57 +0500

    add remove task, add children task

--- apricode-test/src/components/TaskTreeItem/index.tsx
+++ apricode-test/src/components/TaskTreeItem/index.tsx
@@ -2,6 +2,8 @@ import React, { FunctionComponent, useState } from 'react';
 import { observer } from 'mobx-react-lite';
 
 import arrow from '../../icon/icon_sloy.svg';
+import addIcon from '../../icon/add.svg';
+import delIcon from '../../icon/close-sm-svgrepo-com.svg';
 import styles from './TaskTreeItem.module.scss';
 
 import { TaskType } from '../../types/types';
@@ -35,27 +37,47 @@ const TaskTreeItem: FunctionComponent<TaskTreeItemProps> = observer(({ taskItem 
         </Modal>
       )}
       <div className={`${styles.item} ${styles.parent}`}>
-        <div className={styles.checkboxWrap}>
-          <input
-            type="checkbox"
-            className={styles.customCheckbox}
-            name={id}
-            id={id}
-            checked={isCompleted}
-            onChange={() => mainStore.completeToggler(id)}
+        <div className={styles.left}>
+          <div className={styles.checkboxWrap}>
+            <input
+              type="checkbox"
+              className={styles.customCheckbox}
+              name={id}
+              id={id}
+              checked={isCompleted}
+              onChange={() => mainStore.completeToggler(id)}
+            />
+            <label htmlFor={id}></label>
+          </div>
+          <img
+            className={isChildrenShown ? styles.active : styles.arrow}
+            src={arrow}
+            alt=""
+            onClick={childrenToggler}
           />
-          <label htmlFor={id}></label>
+          <h3
+            className={`${styles.title} ${styles.grand}`}
+            onClick={() => mainStore.chooseTask(id)}>
+            {title}
+          </h3>
         </div>
-        <img
-          className={isChildrenShown ? `${styles.arrow} ${styles.active}` : styles.arrow}
-          src={arrow}
-          alt=""
-          onClick={childrenToggler}
-        />
-        <h3 className={`${styles.title} ${styles.grand}`} onClick={() => mainStore.chooseTask(id)}>
-          {title}
-        </h3>
+        <div className={styles.right}>
+          <img className={styles.addIcon} src={addIcon} alt="" onClick={modalToggler} />
+          <img
+            className={styles.delIcon}
+            src={delIcon}
+            alt=""
+            onClick={() => mainStore.removeTask(id)}
+          />
+        </div>
       </div>
+      {children.length > 0 && (
+        <div className={isChildrenShown ? styles.children : styles.hide}>
+          {children.map((child) => (
+            <TaskTreeItem key={child.id} taskItem={child} />
+          ))}
+        </div>
+      )}
     </>
   );
 });
